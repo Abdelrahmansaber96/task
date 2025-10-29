d84d4802a016b7a834118904902ed835de6acd3f
abdelrahman saber <abdusaber130@gmail.com>
2025-10-29 15:03:32 +0300

    new

--- app/api/todos/todoModel.js
+++ app/api/todos/todoModel.js
@@ -2,11 +2,14 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const todosSchema = new Schema({
-  title: String, 
+  title: String,
   author: String,
   completed: Boolean,
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  updatedAt: { type: Date, default: Date.now },
 });
 
-export default  mongoose.model('Todo', todosSchema);
+// التحقق إذا الموديل موجود بالفعل
+const Todo = mongoose.models.Todo || mongoose.model('Todo', todosSchema);
+
+export default Todo;
